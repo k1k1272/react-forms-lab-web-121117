7afc69939a341a26ed40a0681307f42f0b03a024
github username <github email address>
2018-02-19 00:27:59 -0500

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,39 +1,31 @@
 import React from "react";
 
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
     this.state = {
       value: "",
-      hasError: false
+      valid: true
     };
   }
 
   handleText = event => {
     this.setState({
-      value: event.target.value
+      value: event.target.value,
     })
 
-    let errorFound = false
-    let poem = event.target.value.split("\n")
-    if (poem.length === 3) {
-      let lineOne = poem[0]
-      let lineTwo = poem[1]
-      let lineThree = poem[2]
-
-      if (lineOne.split(" ").length !== 5) {
-        hasError: true
-      } else if (lineTwo.split(" ").length !== 3) {
-        hasError: true
-      } else if (lineThree.split(" ").length !== 5) {
-        hasError: true
-      }
+    let poem = event.target.value.split("\n").map(line => line.trim())
+    if (poem && poem.length === 3) {
+      this.setState({
+        valid: (poem[0].split(" ").length === 5 && poem[1].split(" ").length === 3 && poem[2].split(" ").length === 5)
+      })
     } else {
-      hasError: true
-      console.log("not working")
+      this.setState({
+        valid: false
+      })
     }
-
   }
 
   render() {
@@ -41,9 +33,11 @@ class PoemWriter extends React.Component {
     return (
       <div>
         <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleText}/>
-        <div id="poem-validation-error" style={{ color: "red"}}>
-          This poem is not written in the right structure!
-        </div>
+        {!this.state.valid ? (
+          <div id="poem-validation-error" style={{ color: "red"}}>
+            This poem is not written in the right structure!
+          </div>
+        ) : null}
       </div>
     );
   }
